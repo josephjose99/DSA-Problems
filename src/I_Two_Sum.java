f162ec4ed5282e019ac2b2f7b67aa42cdfb29c01
Joseph Jose <j4josephjose@gmail.com>
2025-05-07 19:22:33 +0530

    Inserted comment

--- src/I_Two_Sum.java
+++ src/I_Two_Sum.java
@@ -1,3 +1,15 @@
+/**
+ * Sum Two
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+
 public class I_Two_Sum {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length-1;i++){
